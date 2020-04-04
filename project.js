edbdcb6f43b5eb9baeba8ea5dce07a1433004ebf
Sherman Gerk <40064460+shermangerk@users.noreply.github.com>
2020-04-05 02:35:31 +0800

    Update project.js

--- project.js
+++ project.js
@@ -1,6 +1,6 @@
 function calcVH() {
   var vH = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-  document.getElementById("bg1").setAttribute("style", "height:" + vH + "px;");
+  document.getElementsByClassName("bg1").setAttribute("style", "height:" + vH + "px;");
 }
 calcVH();
 window.addEventListener('onorientationchange', calcVH, true);
