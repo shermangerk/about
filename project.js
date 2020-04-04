7d9fd607d5cec7f8951c9db66a53a8c45a7a0eed
Sherman Gerk <40064460+shermangerk@users.noreply.github.com>
2020-04-05 02:39:28 +0800

    Add files via upload

--- project.js
+++ project.js
@@ -1,6 +1,24 @@
-function calcVH() {
-  var vH = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-  document.getElementsByClassName("bg1").setAttribute("style", "height:" + vH + "px;");
+// Progress Bar
+// let bar= document.querySelector("#progress");
+//
+// window.addEventListener("scroll",()=>{
+//   let max= document.body.scrollHeight - window.innerHeight;
+//   bar.style.width=`${(pageYOffset/max)*100}%`;
+// })
+
+// Sticky Navbar
+// ============================================================================
+window.onscroll = function(){stickyBar()};
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop;
+
+function stickyBar(){
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
-calcVH();
-window.addEventListener('onorientationchange', calcVH, true);
+// ============================================================================
