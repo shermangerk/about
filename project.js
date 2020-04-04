a03b47f0cd015a9735df37fc0f8231d18b895cdc
Sherman Gerk <40064460+shermangerk@users.noreply.github.com>
2020-04-05 02:27:53 +0800

    Update project.js

--- project.js
+++ project.js
@@ -1,24 +1,9 @@
-// Progress Bar
-// let bar= document.querySelector("#progress");
-//
-// window.addEventListener("scroll",()=>{
-//   let max= document.body.scrollHeight - window.innerHeight;
-//   bar.style.width=`${(pageYOffset/max)*100}%`;
-// })
-
-// Sticky Navbar
-// ============================================================================
-window.onscroll = function(){stickyBar()};
-
-var navbar = document.getElementById("navbar");
-
-var sticky = navbar.offsetTop;
-
-function stickyBar(){
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
+function calcVH() {
+    $('.bg1').innerHeight( $(this).innerHeight() );
 }
-// ============================================================================
+(function($) {
+  calcVH();
+  $(window).on('resize orientationchange', function() {
+    calcVH();
+  });
+})(jQuery);
