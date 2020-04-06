d9a39b643e4970d8f1b235e8ed588dad9c8d9d14
Sherman Gerk <40064460+shermangerk@users.noreply.github.com>
2020-04-06 14:00:56 +0800

    Add files via upload

--- project.js
+++ project.js
@@ -1,9 +1,10 @@
     function resized() {
       var h = document.documentElement.clientHeight;
       document.getElementById("container2").style.height = h +"px";
+      document.getElementById("bg1").style.height =h+"px";
 
       var console = document.getElementById('console');
       console.innerHTML += "*Resize! window.innerHeight: " + window.innerHeight + ",  documentElement.clientHeight: " + document.documentElement.clientHeight + "<br>";
-      
+
 
     }
