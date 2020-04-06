3dc131ace0c42519527abfd9e7f0ddc03c127456
Sherman Gerk <40064460+shermangerk@users.noreply.github.com>
2020-04-06 12:04:33 +0800

    Add files via upload

--- project.js
+++ project.js
@@ -1,24 +1,5 @@
-// Progress Bar
-// let bar= document.querySelector("#progress");
-//
-// window.addEventListener("scroll",()=>{
-//   let max= document.body.scrollHeight - window.innerHeight;
-//   bar.style.width=`${(pageYOffset/max)*100}%`;
-// })
 
-// Sticky Navbar
-// ============================================================================
-window.onscroll = function(){stickyBar()};
-
-var navbar = document.getElementById("navbar");
-
-var sticky = navbar.offsetTop;
-
-function stickyBar(){
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-// ============================================================================
+// Calculate height without address bar on mobile
+const appHeight = () => document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`)
+window.addEventListener('resize', appHeight)
+appHeight()
