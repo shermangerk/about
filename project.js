648e3ea1ac6415aab94a56f3ee2e59f6d5b546c8
Sherman Gerk <40064460+shermangerk@users.noreply.github.com>
2020-04-06 14:10:04 +0800

    Add files via upload

--- project.js
+++ project.js
@@ -4,8 +4,8 @@
       document.getElementById("bg1").style.height =h+"px";
 
       var console = document.getElementById('console');
-      console.innerHTML += "*Resize! window.innerHeight: " + window.innerHeight + ",  documentElement.clientHeight: " + document.documentElement.clientHeight + ",  background image height: " + document.getElementById("bg1").style.height + "<br>";
-      console.style.color = "blue";
+      console.innerHTML += "*Resize! window.innerHeight: " + window.innerHeight + ",  documentElement.clientHeight: " + document.documentElement.clientHeight + ",  background image height: " + document.getElementById("bg1").style.height +",  container2 height: " + document.getElementById("container2").style.height + "<br>";
+      console.style.color = "red";
 
 
     }
